bfce4d12c15331f03603516981c9505b886a858d
farihatasnim <tfariha92@gmail.com>
2022-10-18 10:24:10 +0600

    Navbar resolve

--- navbar.php
+++ navbar.php
@@ -1,48 +1,20 @@
-<<<<<<< HEAD
-   <!--Navbar section starts here-->
-   <section class="navbar">
-=======
  <!--Navbar section starts here-->
  <section class="navbar">
->>>>>>> 2645a1d74bd82627e4fef59913c381a1748fab8b
         <div class="container" style="font-size: x-large">
             <div class="logo">
                 <img src="logo.jpg" alt="logo" class="img-responsive">
             </div>
 
-<<<<<<< HEAD
-            <div class="menu text-right">
-=======
             <div class="navmenu text-right">
->>>>>>> 2645a1d74bd82627e4fef59913c381a1748fab8b
                 <ul>
                     <li>
                         <a href="index.php">Home</a>
                     </li>
                     <li>
-<<<<<<< HEAD
-                        <a href="product.php">Ingradient</a>
-                    </li>
-                    <li>
-                        <a href="purchaseform.php">Purchase</a>
-                    </li>
-                    <li>
-                        <a href="stock.php">Stock</a>
-                    </li>
-                    <!-- <li>
-                        <a href="foodform.php">Create Food</a>
-                    </li> -->
-                    <li>
-                        <a href="ordershow.php">Order</a>
-                    </li>
-                    <li>
-                        <a href="foodshowcase2.php">Food Showcase</a>
-=======
                         <a href="manageingradients.php">Manage Ingradients</a>
                     </li>
                     <li>
                         <a href="managefood.php">Manage Food</a>
->>>>>>> 2645a1d74bd82627e4fef59913c381a1748fab8b
                     </li>
                 </ul>
             </div>
@@ -52,9 +24,6 @@
         </div>
     </section>
     <!--Navbar section ends here-->
-<<<<<<< HEAD
-=======
 
 
    
->>>>>>> 2645a1d74bd82627e4fef59913c381a1748fab8b
